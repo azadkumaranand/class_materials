405765e514fae0625275b8997d7ff19492da23b3
azadkumaranand <Khushibansal2411@gmail.com>
2023-09-01 15:25:30 +0530

    add new

--- regex.js
+++ regex.js
@@ -7,12 +7,38 @@ let result = pattern.test(str);
 console.log(result)
 
 
-let phone = "azsdads 84784754645634";
+let phone = "adkkljlhjhklkj9978";
 // azad978@ggmail.com
 // support@mintrago.com
 
 let phone_pattern = /(x|y)\d/;
-phone_pattern = /[a-z]\s\d/;
+phone_pattern = /[a-z]{4}\d/;
 result = phone_pattern.test(phone);
 
 console.log(result)
+
+let email = document.getElementById('email');
+
+
+// alert("hello")
+//input event
+// email.addEventListener("input", function(){
+//     console.log(email.value)
+// })
+//azad98mintra748@gmail.com
+const EmailError = document.getElementById('email-error');
+email.addEventListener("change", function(){
+    // console.log()
+    const email_pattern = /[a-z.]+[0-9]+@[a-z]+\.[a-z]{2}/;
+    let result = email_pattern.test(email.value);
+    console.log(result)
+    if(!result){
+        console.log("please enter a valid email");
+        EmailError.innerText = "please enter a valid email";
+    }else{
+        EmailError.innerText = "";
+    }
+    // if(result){
+
+    // }
+})
